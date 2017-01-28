50feec6c7fda7a794efe232457aa9424d65777d8
Basil <basiltalias92@gmail.com>
2017-01-27 23:42:42 -0500

    Fix issue with multer

--- app.js
+++ app.js
@@ -2,20 +2,20 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
+var expressValidator = require('express-validator');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
 var session = require('express-session');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
+var bodyParser = require('body-parser');
 var multer = require('multer');
 var flash = require('connect-flash');
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
-var expressValidator = require('express-validator')
 var db = mongoose.connection;
 
 
-var index = require('./routes/index');
+var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
@@ -24,7 +24,9 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(multer({dest:'./uploads'}));
+// Handle file uploads
+//var upload = multer({ dest: 'uploads/' })
+var upload = multer({dest: 'uploads/'});
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -32,16 +34,18 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// Handle Express Sessions
 app.use(session({
-  secret:'secret',
-  saveUninitialized: true,
-  resave: true
-}))
-//passport
+    secret:'secret',
+    saveUninitialized: true,
+    resave: true
+}));
+
+// passport
 app.use(passport.initialize());
 app.use(passport.session());
 
-//express validator
+// Validator
 app.use(expressValidator({
   errorFormatter: function(param, msg, value) {
       var namespace = param.split('.')
@@ -59,15 +63,22 @@ app.use(expressValidator({
   }
 }));
 
-app.use(flash);
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use(flash());
 app.use(function (req, res, next) {
   res.locals.messages = require('express-messages')(req, res);
   next();
 });
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+app.get('*', function(req, res, next){
+    res.locals.user = req.user || null;
+    next();
+});
+
+app.use('/', routes);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
@@ -77,15 +88,29 @@ app.use(function(req, res, next) {
   next(err);
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// error handlers
 
-  // render the error page
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
+
 
 module.exports = app;
