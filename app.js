e598af2fe68bb47a149ecbc6ad119ce70570ef1f
Basil <basiltalias92@gmail.com>
2017-01-27 23:14:39 -0500

    updated flash

--- app.js
+++ app.js
@@ -59,6 +59,11 @@ app.use(expressValidator({
   }
 }));
 
+app.use(flash);
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
